e6158391ed532b172c3460061865431013fb34da
shch <2032239378@qq.com>
2020-11-06 08:48:37 +0800

    init

--- README.md
+++ README.md
@@ -1,30 +1,24 @@
+# onlineeditor
 
-
-### Vue&Code Mirror 编辑器
-
-1. Code Mirror 介绍
-2. 页面布局
-3. 运行
-4. 导入文件
-5. 注释
-6. 保存
-7. 清楚
-8. 下载
-9. 复制
-10. emmit语法
-11. html 模版
-12. css 模版
-13. javascript 模版
-14. 保存
-
-### Project setup
+## Project setup
+```
 npm install
+```
 
 ### Compiles and hot-reloads for development
+```
 npm run serve
+```
 
 ### Compiles and minifies for production
+```
 npm run build
+```
 
 ### Lints and fixes files
+```
 npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+    <textarea ref="editor"></textarea>
+</template>
+
+<script>
+
+import 'codemirror/lib/codemirror.css';
+import codemirror from "codemirror";
+
+export default {
+  name: 'Home',
+  data:function (){
+    return{
+      editor:'',
+      options:{
+        lineNumbers: true,
+        line: true,
+        tabSize: 2,
+      },
+    }
+  },
+  methods:{
+    initeditor(){
+      this.editor = codemirror.fromTextArea(this.$refs.editor, this.options);
+    }
+  },
+  mounted(){
+    this.initeditor();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
