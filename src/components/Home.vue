9724ea893b0f3aeb17dcf357920520b66c866414
shch <2032239378@qq.com>
2020-11-09 22:10:44 +0800

    项目布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,24 @@
 <template>
-    <textarea ref="editor"></textarea>
+<div class="editorwrap">
+    <div class="left">
+      <div class="editor">
+        <div class="editorheader">HTML</div>
+         <textarea ref="htmleditor"></textarea>
+      </div>
+      <div class="editor">
+        <div class="editorheader">CSS</div>
+         <textarea ref="csseditor"></textarea>
+      </div>
+      <div class="editor">
+        <div class="editorheader">JavaScript</div>
+         <textarea ref="jseditor"></textarea>
+      </div>
+    </div>
+    <div class="right">
+      <div class="editorheader">结果</div>
+      <iframe class="iframe" ref="iframe" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,21 +26,46 @@
 import 'codemirror/lib/codemirror.css';
 import codemirror from "codemirror";
 
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/htmlmixed/htmlmixed';
+
 export default {
   name: 'Home',
   data:function (){
     return{
-      editor:'',
-      options:{
+      htmleditor:'',
+      htmloptions:{
         lineNumbers: true,
         line: true,
         tabSize: 2,
+        mode: 'htmlmixed',
+        theme: 'material',
+      },
+      csseditor:'',
+      cssoptions:{
+        lineNumbers: true,
+        line: true,
+        tabSize: 2,
+        mode: 'css',
+        theme: 'material',
+      },
+      jseditor:'',
+      jsoptions:{
+        lineNumbers: true,
+        line: true,
+        tabSize: 2,
+        mode: 'javascript',
+        theme: 'material',
       },
     }
   },
   methods:{
     initeditor(){
-      this.editor = codemirror.fromTextArea(this.$refs.editor, this.options);
+      this.htmleditor = codemirror.fromTextArea(this.$refs.htmleditor, this.htmloptions);
+      this.csseditor = codemirror.fromTextArea(this.$refs.csseditor, this.cssoptions);
+      this.jseditor = codemirror.fromTextArea(this.$refs.jseditor, this.jsoptions);
     }
   },
   mounted(){
@@ -32,5 +76,35 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+.editorwrap{
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  .left,.right{
+    flex:1;
+    height: 100%;
+  }
+  .left{
+    display: flex;
+    flex-direction: column;
+  }
+  .editor{
+    flex:1;
+  }
+  .editorheader{
+    height: 30px;
+    background-color:#808080;
+    color:#fff;
+    display: flex;
+    align-items: center;
+    padding-left: 20px;
+  }
+  .iframe{
+    width: 100%;
+    height: 100%;
+  }
+}
+
 
 </style>
